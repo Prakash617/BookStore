6ec9c6ef99c1d7a1b996d65836b9bc4b5cb5ae11
shasank.shakya <shasank.shakya@cellcom.net.np>
2024-03-25 17:46:05 +0545

    navbar and its components

---
+++ components/Layout/LanguageSwitcher.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import Link from "next/link"
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger
+} from "@/components/ui/navigation-menu"
+import { cn } from '@/lib/utils'
+import { Globe } from 'lucide-react'
+
+type Props = {}
+
+
+export const preventHover = (event: any) => {
+    const e = event as Event
+    if (window.innerWidth < 1024) e.preventDefault()
+  }
+
+const LanguageSwitcher = (props: Props) => {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger className='text-lg focus:bg-transparent' onPointerMove={event => event.preventDefault()} onPointerLeave={event => event.preventDefault()}> <Globe className='mr-2' />EN</NavigationMenuTrigger>
+                    <NavigationMenuContent onPointerMove={event => event.preventDefault()} onPointerLeave={event => event.preventDefault()}>
+                        <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                            <li className="row-span-3">
+                                <NavigationMenuLink asChild>
+                                    <a
+                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                        href="/"
+                                    >
+
+                                        <div className="mb-2 mt-4 text-lg font-medium">
+                                            shadcn/ui
+                                        </div>
+                                        <p className="text-sm leading-tight text-muted-foreground">
+                                            Beautifully designed components that you can copy and
+                                            paste into your apps. Accessible. Customizable. Open
+                                            Source.
+                                        </p>
+                                    </a>
+                                </NavigationMenuLink>
+                            </li>
+                            <ListItem href="/docs" title="Introduction">
+                                Re-usable components built using Radix UI and Tailwind CSS.
+                            </ListItem>
+                            <ListItem href="/docs/installation" title="Installation">
+                                How to install dependencies and structure your app.
+                            </ListItem>
+                            <ListItem href="/docs/primitives/typography" title="Typography">
+                                Styles for headings, paragraphs, lists...etc
+                            </ListItem>
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
+
+export default LanguageSwitcher
+
+
+const ListItem = React.forwardRef<
+    React.ElementRef<"a">,
+    React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+    return (
+        <li>
+            <NavigationMenuLink asChild>
+                <a
+                    ref={ref}
+                    className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        className
+                    )}
+                    {...props}
+                >
+                    <div className="text-sm font-medium leading-none">{title}</div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        {children}
+                    </p>
+                </a>
+            </NavigationMenuLink>
+        </li>
+    )
+})
+ListItem.displayName = "ListItem"
