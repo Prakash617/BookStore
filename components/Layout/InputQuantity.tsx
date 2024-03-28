1d081dc82fdeb914dd7ea21094043ad894a4af62
Prakash617 <prakashthapa617@gmail.com>
2024-03-28 19:49:54 +0545

    change in cart

--- components/Layout/InputQuantity.tsx
+++ components/Layout/InputQuantity.tsx
@@ -24,29 +24,37 @@ const InputQuantity = (props: Props) => {
               <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
             </svg>
           </div> */}
-          <div className="flex justify-center space-x-3 w-1/5">
-                                <button onClick={decrementQuantity}
-                                    className="group rounded-[50px] border border-gray-200 shadow-sm shadow-transparent p-2.5 flex items-center justify-center bg-white transition-all duration-500 hover:shadow-gray-200 hover:bg-gray-50 hover:border-gray-300 focus-within:outline-gray-300">
-                                    <svg className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
-                                        width="18" height="19" viewBox="0 0 18 19" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M4.5 9.5H13.5" stroke="" stroke-width="1.6" stroke-linecap="round"
-                                            stroke-linejoin="round" />
-                                    </svg>
-                                </button>
-                                <input type="text" id="number" value={quantity}
-                                    className="border border-gray-200 rounded-full w-10 aspect-square outline-none text-gray-900 font-semibold text-sm py-1.5 px-3 bg-gray-100  text-center"
-                                    />
-                                <button onClick={()=> setQuantity(quantity+1)}
-                                    className="group rounded-[50px] border border-gray-200 shadow-sm shadow-transparent p-2.5 flex items-center justify-center bg-white transition-all duration-500 hover:shadow-gray-200 hover:bg-gray-50 hover:border-gray-300 focus-within:outline-gray-300">
-                                    <svg className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
-                                        width="18" height="19" viewBox="0 0 18 19" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M3.75 9.5H14.25M9 14.75V4.25" stroke="" stroke-width="1.6"
-                                            stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                </button>
-                            </div>
+          <div className="flex items-center h-full">
+                                    <button onClick={decrementQuantity} 
+                                        className="group rounded-l-full px-5 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-50 hover:border-gray-300 hover:shadow-gray-300 focus-within:outline-gray-300">
+                                        <svg className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
+                                            xmlns="http://www.w3.org/2000/svg" width="22" height="22"
+                                            viewBox="0 0 22 22" fill="none">
+                                            <path d="M16.5 11H5.5" stroke="" stroke-width="1.6"
+                                                stroke-linecap="round" />
+                                            <path d="M16.5 11H5.5" stroke="" stroke-opacity="0.2" stroke-width="1.6"
+                                                stroke-linecap="round" />
+                                            <path d="M16.5 11H5.5" stroke="" stroke-opacity="0.2" stroke-width="1.6"
+                                                stroke-linecap="round" />
+                                        </svg>
+                                    </button>
+                                    <input type="text" value={quantity} 
+                                        className="border-y border-gray-200 outline-none text-gray-900 font-semibold text-lg w-full max-w-[73px] min-w-[60px] placeholder:text-gray-900 py-[15px]  text-center bg-transparent"
+                                        placeholder="1" />
+                                    <button onClick={()=> setQuantity(quantity+1)}
+                                        className="group rounded-r-full px-5 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-50 hover:border-gray-300 hover:shadow-gray-300 focus-within:outline-gray-300">
+                                        <svg className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
+                                            xmlns="http://www.w3.org/2000/svg" width="22" height="22"
+                                            viewBox="0 0 22 22" fill="none">
+                                            <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" stroke-width="1.6"
+                                                stroke-linecap="round" />
+                                            <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" stroke-opacity="0.2"
+                                                stroke-width="1.6" stroke-linecap="round" />
+                                            <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" stroke-opacity="0.2"
+                                                stroke-width="1.6" stroke-linecap="round" />
+                                        </svg>
+                                    </button>
+                                </div>
      </>
   )
 }
