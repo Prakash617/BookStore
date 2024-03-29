4e27026e9d351f8c20e6b7f054030d5e93053963
shasank.shakya <shasank.shakya@cellcom.net.np>
2024-03-29 13:21:22 +0545

    added best seller

--- app/[locale]/(home)/BestSeller.tsx
+++ app/[locale]/(home)/BestSeller.tsx
@@ -1,3 +1,4 @@
+import ProductCard from '@/components/ProductCard';
 import { useTranslations } from 'next-intl';
 import React from 'react'
 
@@ -12,9 +13,13 @@ const BestSeller = (props: Props) => {
             <div className='px-32 py-10 space-y-2'>
                 <h2 className='text-3xl font-semibold'>{t('title')}</h2>
                 <h4 className='text-gray-600'>{t('sub-title')}</h4>
+                <div className='grid grid-cols-5 gap-10 pt-5'>
+                    {
+                        Array(5).fill(".").map((_, index: number) => {
+                            return (<ProductCard title='Bhagavad Gita As It Is' imageUrl='/deleteBookBhagwatGita.webp' price='670' author='Bhaktivedanta Swami' />)
+                        })
 
-
-                <div className='grid'>
+                    }
 
                 </div>
             </div>
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,11 +1,32 @@
+import Image from 'next/image'
 import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import { useTranslations } from 'next-intl'
 
-type Props = {}
+type Props = {
+    imageUrl: string
+    title: string
+    price: string
+    author: string
+}
 
 const ProductCard = (props: Props) => {
-  return (
-    <div>ProductCard</div>
-  )
+    const { imageUrl, title, price, author } = props
+
+    const t = useTranslations('components.productCard');
+
+    return (
+        <div className='group'>
+            <Link href={'#'} >
+                <Image src={imageUrl} alt='' width={300} height={300} className='mb-3 rounded-sm' />
+                <span><h3 className='font-semibold text-lg'>{title}</h3> by <span className='text-gray-600'>{author}</span></span>
+                <h3 className='font-semibold text-lg'>{t('currency')} {price}</h3>
+
+            </Link>
+            <Button className='w-full mt-5 invisible group-hover:visible'>{t('addToCart')}</Button>
+        </div>
+    )
 }
 
 export default ProductCard
