3228073109d0d2ccea70992c249179a1d8bbdc15
Prakash617 <prakashthapa617@gmail.com>
2024-04-02 21:10:11 +0545

    order

--- app/[locale]/cart/page.tsx
+++ app/[locale]/cart/page.tsx
@@ -7,9 +7,9 @@ type Props = {};
 const Cart = (props: Props) => {
   return (
     <>
-                                                      <section
-        className=" relative z-10 after:contents-[''] after:absolute after:z-0 after:h-full xl:after:w-1/3 after:top-0 after:right-0 after:bg-gray-50">
-        <div className="w-full max-w-7xl px-4 md:px-5 lg-6 mx-auto relative z-10">
+        <section
+        className=" relative mt-8 after:contents-[''] after:absolute after:z-0 after:h-full xl:after:w-1/3 after:top-0 after:right-0 ">
+        <div className="w-full max-w-7xl px-4 md:px-5 lg-6 mx-auto relative ">
             <div className="grid grid-cols-12">
                 <div
                     className="col-span-12 xl:col-span-8 lg:pr-8 pt-14 pb-8 lg:py-24 w-full max-xl:max-w-3xl max-xl:mx-auto">
@@ -116,7 +116,7 @@ const Cart = (props: Props) => {
                     </div>
                 </div>
                 <div
-                    className=" col-span-12 xl:col-span-4 bg-gray-50 w-full max-xl:px-6 max-w-3xl xl:max-w-lg mx-auto lg:pl-8 py-24">
+                    className=" col-span-12 xl:col-span-4 bg-gray-50 w-full max-xl:px-6 max-w-3xl xl:max-w-lg mx-auto lg:px-8 py-24">
                     <h2 className="font-manrope font-bold text-3xl leading-10 text-black pb-8 border-b border-gray-300">
                         Order Summary</h2>
                     <div className="mt-8">
